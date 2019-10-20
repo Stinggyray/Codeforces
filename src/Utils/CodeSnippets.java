216561875368e4b1d9ae4806cead29c203d1b335
Raymond Zhao <stinggyray@gmail.com>
2019-10-20 00:35:33 -0500

    added project config files

--- src/Utils/CodeSnippets.java
+++ src/Utils/CodeSnippets.java
@@ -18,7 +18,20 @@ public class CodeSnippets {
         sc.close();
     }
 
-    public static boolean isPrime(int n) {
+    //I don't know how this works. It's also really inefficient.
+    public static boolean isPrimeShort(int n) {
         return !new String(new char[n]).matches(".?|(..+?)\\1+");
+    }
+
+    //This one works better.
+    public static boolean isPrime(long n) {
+        if(n < 2) return false;
+        if(n == 2 || n == 3) return true;
+        if(n%2 == 0 || n%3 == 0) return false;
+        long sqrtN = (long)Math.sqrt(n)+1;
+        for(long i = 6L; i <= sqrtN; i += 6) {
+            if(n%(i-1) == 0 || n%(i+1) == 0) return false;
+        }
+        return true;
     }
 }
